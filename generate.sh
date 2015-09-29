5fdd8982cb8286695eb785159b29c2b8cde36ec9
Manfred Moser <manfred@simpligility.com>
2015-09-28 22:45:06 -0700

    renamed css and adapated build script

--- generate.sh
+++ generate.sh
@@ -9,7 +9,7 @@ set -u
 function render {
     echo "--------------------"
     echo -e "Started Processing $1\n"
-    asciidoc -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/src/simpligility-slidy.css -o $1.html src/$1.asciidoc
+    asciidoc -a data-uri --backend slidy2 --attribute stylesheet=`pwd`/src/our-slidy.css -o $1.html src/$1.asciidoc
     echo "  Built $1.html"
     
 #    a2x -k -fpdf --dblatex-opts="-P toc.section.depth=1 -P latex.output.revhistory=0 -P doc.publisher.show=0" -D `pwd` src/$1.asciidoc
